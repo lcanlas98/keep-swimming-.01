577ac946894147c389f0e91070af3528c14572f5
sneL <lcanlas1998@gmail.com>
2021-12-21 04:19:21 -0800

    starting line

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'bg-main': "url('/image/backgrounds/grass1.jpg')",
+      }
+    },
+  },
+  plugins: [
+  ],
+};
